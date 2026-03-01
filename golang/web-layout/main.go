3132d5078cd2191ec50d36103b9a0bcefe4d267d
alkalax <57093460+alkalax@users.noreply.github.com>
2026-03-01 20:18:15 +0100

    Add header with border and main

--- golang/web-layout/main.go
+++ golang/web-layout/main.go
@@ -5,10 +5,33 @@ import (
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type model struct {
-	msg string
+	width  int
+	height int
+	header header
+	main   mainDiv
+}
+
+type header struct {
+	content string
+	width   int
+	height  int
+}
+
+type mainDiv struct {
+	content string
+	width   int
+	height  int
+}
+
+func initialModel() model {
+	return model{
+		header: header{content: "Title Here"},
+		main:   mainDiv{content: "Some text here"},
+	}
 }
 
 func (m model) Init() tea.Cmd {
@@ -17,6 +40,10 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.width = msg.Width
+		m.height = msg.Height
+		return m, nil
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "q":
@@ -27,12 +54,24 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, nil
 }
 
+func (h header) View(width, height int) string {
+	return lipgloss.NewStyle().
+		Width(width).
+		Height(height).
+		Align(lipgloss.Center, lipgloss.Center).
+		Border(lipgloss.NormalBorder()).
+		Render(h.content)
+}
+
 func (m model) View() string {
-	return m.msg
+	header := m.header.View(m.width-2, m.height/8)
+	main := m.main.content
+
+	return lipgloss.JoinVertical(lipgloss.Top, header, main)
 }
 
 func main() {
-	p := tea.NewProgram(model{msg: "hello"}, tea.WithAltScreen())
+	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)

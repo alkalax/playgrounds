69c57be0f38137d3f3eafaa00a62702a594af145
alkalax <57093460+alkalax@users.noreply.github.com>
2026-03-01 20:05:36 +0100

    Add initial files

---
+++ golang/web-layout/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type model struct {
+	msg string
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "ctrl+c", "q":
+			return m, tea.Quit
+		}
+	}
+
+	return m, nil
+}
+
+func (m model) View() string {
+	return m.msg
+}
+
+func main() {
+	p := tea.NewProgram(model{msg: "hello"}, tea.WithAltScreen())
+	if _, err := p.Run(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}

7f0de19c15622308122cd3c46c813c84ac61b63b
alkalax <57093460+alkalax@users.noreply.github.com>
2026-03-09 18:32:18 +0100

    Add progress bar

--- golang/progress-multiple/main.go
+++ golang/progress-multiple/main.go
@@ -3,20 +3,31 @@ package main
 import (
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/charmbracelet/bubbles/progress"
 	tea "github.com/charmbracelet/bubbletea"
 )
 
 type model struct {
-	content string
+	percent  float64
+	progress progress.Model
+}
+
+type tickMsg time.Time
+
+func tick() tea.Cmd {
+	return tea.Tick(time.Millisecond, func(t time.Time) tea.Msg {
+		return tickMsg(t)
+	})
 }
 
 func initialModel() model {
-	return model{content: "hello"}
+	return model{progress: progress.New()}
 }
 
 func (m model) Init() tea.Cmd {
-	return nil
+	return tick()
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -26,13 +37,20 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "ctrl+c", "q":
 			return m, tea.Quit
 		}
+	case tickMsg:
+		m.percent += 0.0001
+		if m.percent > 1.0 {
+			m.percent = 1.0
+			return m, nil
+		}
+		return m, tick()
 	}
 
 	return m, nil
 }
 
 func (m model) View() string {
-	return m.content
+	return m.progress.ViewAs(m.percent)
 }
 
 func main() {

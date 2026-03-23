0b14d7a16de0c8a69b9926502f651958ad88174f
alkalax <57093460+alkalax@users.noreply.github.com>
2026-03-23 18:34:40 +0100

    Add dashboard and namespaces in sidebar

--- golang/pod-viewer/main.go
+++ golang/pod-viewer/main.go
@@ -3,17 +3,42 @@ package main
 import (
 	"fmt"
 	"os"
-	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type model struct {
+	dashboard Dashboard
+}
+
+type Dashboard struct {
+	width   int
+	height  int
+	sidebar Sidebar
+	main    Main
+}
+
+type Sidebar struct {
+	width      int
+	height     int
 	namespaces []string
 }
 
+type Main struct {
+	width  int
+	height int
+}
+
 func initialModel() model {
-	return model{namespaces: getNamespaces()}
+	return model{
+		Dashboard{
+			sidebar: Sidebar{
+				namespaces: getNamespaces(),
+			},
+			main: Main{},
+		},
+	}
 }
 
 func (m model) Init() tea.Cmd {
@@ -22,6 +47,9 @@ func (m model) Init() tea.Cmd {
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.dashboard.width = msg.Width
+		m.dashboard.height = msg.Height
 	case tea.KeyMsg:
 		switch msg.String() {
 		case "ctrl+c", "q":
@@ -32,8 +60,41 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, nil
 }
 
+func (s Sidebar) View(width, height int) string {
+	renderedNamespaces := []string{}
+	for _, ns := range s.namespaces {
+		renderedNamespaces = append(renderedNamespaces,
+			lipgloss.NewStyle().
+				Width(width-2).
+				Align(lipgloss.Center).
+				Render(ns),
+		)
+	}
+
+	return lipgloss.NewStyle().
+		Width(width - 2).
+		Height(height - 2).
+		Border(lipgloss.RoundedBorder()).
+		Render(lipgloss.JoinVertical(lipgloss.Top, renderedNamespaces...))
+}
+
+func (m Main) View(width, height int) string {
+	return lipgloss.NewStyle().
+		Width(width - 2).
+		Height(height - 2).
+		Border(lipgloss.RoundedBorder()).
+		Render("")
+}
+
+func (d Dashboard) View() string {
+	renderedSidebar := d.sidebar.View(d.width*1/5, d.height)
+	renderedMain := d.main.View(d.width*4/5, d.height)
+
+	return lipgloss.JoinHorizontal(lipgloss.Left, renderedSidebar, renderedMain)
+}
+
 func (m model) View() string {
-	return strings.Join(m.namespaces, "\n")
+	return m.dashboard.View()
 }
 
 func main() {

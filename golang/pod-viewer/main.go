f1f3825d0861df044584179f998f5ae27c6af581
alkalax <57093460+alkalax@users.noreply.github.com>
2026-03-23 18:17:27 +0100

    Move getNamespaces to utils

--- golang/pod-viewer/main.go
+++ golang/pod-viewer/main.go
@@ -1,51 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"io"
-	"net/http"
 	"os"
 	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
 
-var server = "http://127.0.0.1:8001"
-
 type model struct {
 	namespaces []string
-}
-
-func getNamespaces() []string {
-	url := server + "/api/v1/namespaces"
-	resp, err := http.Get(url)
-	if err != nil {
-		panic(err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		body, _ := io.ReadAll(resp.Body)
-		panic(fmt.Sprintf("error: %s", body))
-	}
-
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		panic(err)
-	}
-
-	var nsList NamespaceList
-	if err := json.Unmarshal(body, &nsList); err != nil {
-		panic(err)
-	}
-
-	namespaces := []string{}
-	for _, ns := range nsList.Items {
-		namespaces = append(namespaces, ns.Metadata.Name)
-	}
-
-	return namespaces
 }
 
 func initialModel() model {

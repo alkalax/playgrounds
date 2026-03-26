94391b93a50d5f8fde47f695219febe56e93cd2a
alkalax <57093460+alkalax@users.noreply.github.com>
2026-03-26 08:43:52 +0100

    Add pod log streamer with debug logging

--- golang/log-streamer/main.go
+++ golang/log-streamer/main.go
@@ -1,22 +1,69 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"log"
+	"net/http"
 	"os"
+	"strings"
+	"time"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 type model struct {
-	content string
+	logs   []string
+	stream chan LogMsg
+}
+
+type LogMsg struct {
+	Line string
+}
+
+func startStream(ch chan<- LogMsg) tea.Cmd {
+	return func() tea.Msg {
+		resp, err := http.Get("http://127.0.0.1:8001/api/v1/namespaces/default/pods/logger/log?follow=true")
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		reader := bufio.NewReader(resp.Body)
+		for {
+			line, _ := reader.ReadString('\n')
+			ch <- LogMsg{Line: line}
+			log.Println("Sent line")
+		}
+	}
+}
+
+func startSimpleStream(ch chan<- LogMsg) tea.Cmd {
+	return func() tea.Msg {
+		i := 0
+		for {
+			time.Sleep(1 * time.Second)
+			ch <- LogMsg{Line: fmt.Sprintf("log line %d", i)}
+			log.Println("Sent line")
+			i++
+		}
+	}
 }
 
 func initialModel() model {
-	return model{content: "test"}
+	return model{logs: []string{}, stream: make(chan LogMsg)}
 }
 
 func (m model) Init() tea.Cmd {
-	return nil
+	return tea.Batch(startStream(m.stream), waitForMsg(m.stream))
+}
+
+func waitForMsg(ch <-chan LogMsg) tea.Cmd {
+	return func() tea.Msg {
+		log.Println("waiting")
+		return <-ch
+	}
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
@@ -26,17 +73,27 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		case "ctrl+c", "q":
 			return m, tea.Quit
 		}
+	case LogMsg:
+		log.Println("Read line")
+		m.logs = append(m.logs, strings.Trim(msg.Line, "\n"))
+		return m, waitForMsg(m.stream)
 	}
 
 	return m, nil
 }
 
 func (m model) View() string {
-	return m.content
+	log.Printf("Update: %d\n", len(m.logs))
+	return lipgloss.JoinVertical(lipgloss.Top, m.logs...)
 }
 
 func main() {
-	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
+	f, err := tea.LogToFile("debug.log", "debug")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	p := tea.NewProgram(initialModel())
 	if _, err := p.Run(); err != nil {
 		fmt.Println(err)
 		os.Exit(1)

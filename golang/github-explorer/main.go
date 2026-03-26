366cc56638cbff774036b71b3b832d62d835cf60
alkalax <57093460+alkalax@users.noreply.github.com>
2026-03-26 17:45:07 +0100

    Use token and fetch repo languages

--- golang/github-explorer/main.go
+++ golang/github-explorer/main.go
@@ -11,12 +11,14 @@ import (
 type Client struct {
 	http    *http.Client
 	baseUrl string
+	token   string
 }
 
-func NewClient(baseUrl string) *Client {
+func NewClient(baseUrl, token string) *Client {
 	return &Client{
 		http:    http.DefaultClient,
 		baseUrl: baseUrl,
+		token:   token,
 	}
 }
 
@@ -24,14 +26,28 @@ type Repo struct {
 	Name         string `json:"name"`
 	Description  string `json:"description"`
 	LanguagesUrl string `json:"languages_url"`
+	Languages    []string
 }
 
 func (c *Client) listRepos(user string) ([]Repo, error) {
-	resp, err := c.http.Get(fmt.Sprintf("%s/users/%s/repos", c.baseUrl, user))
+	req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("%s/users/%s/repos", c.baseUrl, user), nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Accept", "application/vnd.github+json")
+	req.Header.Set("User-Agent", "github-explorer")
+	req.Header.Set("Authorization", "token "+c.token)
+
+	resp, err := c.http.Do(req)
 	if err != nil {
 		return nil, err
 	}
 	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		body, _ := io.ReadAll(resp.Body)
+		return nil, fmt.Errorf("github status %d: %s", resp.StatusCode, string(body))
+	}
 
 	body, err := io.ReadAll(resp.Body)
 	if err != nil {
@@ -43,11 +59,42 @@ func (c *Client) listRepos(user string) ([]Repo, error) {
 		return nil, err
 	}
 
+	for i := range repos {
+		langReq, err := http.NewRequest(http.MethodGet, repos[i].LanguagesUrl, nil)
+		if err != nil {
+			return nil, err
+		}
+		langReq.Header.Set("Accept", "application/vnd.github+json")
+		langReq.Header.Set("User-Agent", "github-explorer")
+		langReq.Header.Set("Authorization", "token "+c.token)
+
+		langResp, err := c.http.Do(langReq)
+		if err != nil {
+			return nil, err
+		}
+		defer langResp.Body.Close()
+
+		langBody, err := io.ReadAll(langResp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		var languages map[string]int
+		if err = json.Unmarshal(langBody, &languages); err != nil {
+			return nil, err
+		}
+
+		repos[i].Languages = []string{}
+		for language := range languages {
+			repos[i].Languages = append(repos[i].Languages, language)
+		}
+	}
+
 	return repos, nil
 }
 
 func main() {
-	client := NewClient("https://api.github.com")
+	client := NewClient("https://api.github.com", os.Getenv("GITHUB_TOKEN"))
 	repos, err := client.listRepos("alkalax")
 	if err != nil {
 		fmt.Println(err)
@@ -55,6 +102,6 @@ func main() {
 	}
 
 	for _, repo := range repos {
-		fmt.Println(repo.Name)
+		fmt.Println(repo.Name, repo.Languages)
 	}
 }

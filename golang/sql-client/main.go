7d873ab260b7ded7ead498e11de03bc12558ad11
alkalax <57093460+alkalax@users.noreply.github.com>
2026-03-29 16:40:13 +0200

    Add user select and insert

--- golang/sql-client/main.go
+++ golang/sql-client/main.go
@@ -1,11 +1,44 @@
 package main
 
 import (
+	"bufio"
 	"database/sql"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 
 	_ "modernc.org/sqlite"
 )
+
+type User struct {
+	id   int
+	name string
+	age  int
+}
+
+func userExists(name string, db *sql.DB) ([]User, error) {
+	rows, err := db.Query("SELECT Id, Name, Age FROM Users WHERE Name = ?", name)
+	if err != nil {
+		return nil, err
+	}
+
+	var users []User
+	for rows.Next() {
+		var user User
+		if err = rows.Scan(&user.id, &user.name, &user.age); err != nil {
+			return nil, err
+		}
+		users = append(users, user)
+	}
+
+	return users, nil
+}
+
+func createUser(name string, age int, db *sql.DB) error {
+	_, err := db.Exec("INSERT INTO Users (name, age) VALUES (?, ?)", name, age)
+	return err
+}
 
 func main() {
 	db, err := sql.Open("sqlite", "practice.db")
@@ -14,14 +47,47 @@ func main() {
 	}
 	defer db.Close()
 
-	_, err = db.Exec(`
+	if _, err = db.Exec(`
 	CREATE TABLE IF NOT EXISTS Users (
 		Id INTEGER PRIMARY KEY,
 		Name TEXT,
 		Age INTEGER
 	)
-	`)
+	`); err != nil {
+		panic(err)
+	}
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print("Enter name: ")
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSpace(name)
+	users, err := userExists(name, db)
 	if err != nil {
 		panic(err)
+	}
+
+	if len(users) > 0 {
+		for i, user := range users {
+			if i > 0 {
+				fmt.Println("========================")
+			}
+			fmt.Println("User found.")
+			fmt.Println("Id:", user.id)
+			fmt.Println("Name:", user.name)
+			fmt.Println("Age:", user.age)
+		}
+	} else {
+		fmt.Printf("User %s not found. Creating...\n", name)
+		fmt.Print("Age? ")
+		ageStr, _ := reader.ReadString('\n')
+		age, err := strconv.Atoi(strings.TrimSpace(ageStr))
+		if err != nil {
+			panic(err)
+		}
+		if err = createUser(name, age, db); err != nil {
+			panic(err)
+		}
+
+		fmt.Println("User created.")
 	}
 }

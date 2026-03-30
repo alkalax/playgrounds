ef45541cd325f08a9ec60d3ea8539293e25630b8
alkalax <57093460+alkalax@users.noreply.github.com>
2026-03-30 19:25:06 +0200

    Add simple fan-out/fan-in

--- golang/concurrency-simple/main.go
+++ golang/concurrency-simple/main.go
@@ -13,15 +13,29 @@ func worker(id int, ch chan string) {
 }
 
 func main() {
-	ch := make(chan string)
+	//ch := make(chan string)
 
-	n := 5
-	for i := 1; i <= n; i++ {
-		go worker(i, ch)
+	//n := 5
+	//for i := 1; i <= n; i++ {
+	//	go worker(i, ch)
+	//}
+
+	//for i := 1; i <= n; i++ {
+	//	msg := <-ch
+	//	fmt.Println(msg)
+	//}
+
+	ch := make(chan int)
+	jobs := []int{1, 2, 3, 4, 5, 6, 7}
+	for _, job := range jobs {
+		go func(j int) {
+			time.Sleep(time.Duration(j) * time.Second)
+			ch <- j * 2
+		}(job)
 	}
 
-	for i := 1; i <= n; i++ {
-		msg := <-ch
-		fmt.Println(msg)
+	for i := 0; i < len(jobs); i++ {
+		result := <-ch
+		fmt.Println(result)
 	}
 }

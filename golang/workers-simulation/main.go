a437b893a02b02240a8abdb21ec91e4d8c2c371f
alkalax <57093460+alkalax@users.noreply.github.com>
2026-04-03 13:47:34 +0200

    Add citizen struct and print time

--- golang/workers-simulation/main.go
+++ golang/workers-simulation/main.go
@@ -2,31 +2,64 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
 type World struct {
-	workers []int
+	citizens []*Citizen
 }
 
-func NewWorld(workers []int) *World {
+type Citizen struct {
+	Id     int
+	Name   string
+	Job    string
+	Status string
+}
+
+func NewWorld() *World {
+	citizens := []*Citizen{
+		{
+			Id:     1,
+			Name:   "Amy",
+			Job:    "Laborer",
+			Status: "working",
+		},
+		{
+			Id:     2,
+			Name:   "John",
+			Job:    "Laborer",
+			Status: "working",
+		},
+		{
+			Id:     3,
+			Name:   "Robert",
+			Job:    "Laborer",
+			Status: "idle",
+		},
+	}
+
 	return &World{
-		workers: workers,
+		citizens: citizens,
 	}
 }
 
-func (w *World) Display() {
+func (w *World) Display(time time.Time) {
 	fmt.Print("\033[H\033[2J") // clear terminal
-	for _, worker := range w.workers {
-		fmt.Printf("worker %d: idle\n", worker)
+	timeStr := time.String()
+	fmt.Println(timeStr)
+	fmt.Println(strings.Repeat("=", len(timeStr)))
+
+	for _, citizen := range w.citizens {
+		fmt.Printf("%s %s: %s\n", citizen.Job, citizen.Name, citizen.Status)
 	}
 }
 
 func main() {
-	world := NewWorld([]int{1, 2, 3})
+	world := NewWorld()
 
 	for {
-		world.Display()
+		world.Display(time.Now())
 		time.Sleep(time.Second)
 	}
 }

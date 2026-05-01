037e281c1a1ac44ba9c27deec31b205936890b80
alkalax <57093460+alkalax@users.noreply.github.com>
2026-05-01 10:44:23 +0200

    Add extra log

--- .github/actions/js-action/index.mjs
+++ .github/actions/js-action/index.mjs
@@ -3,6 +3,7 @@ import * as core from "@actions/core";
 try {
   const nameToGreet = core.getInput('who-to-greet');
   console.log(`Hello ${nameToGreet}!`);
+  console.log("Extra log")
 } catch (err) {
   core.setFailed(err.message);
 }

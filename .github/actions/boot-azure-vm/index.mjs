ab18a715b2a2c61182368aa0ac844ef7b620f06f
alkalax <57093460+alkalax@users.noreply.github.com>
2026-05-01 20:08:35 +0200

    Test new action

--- .github/actions/boot-azure-vm/index.mjs
+++ .github/actions/boot-azure-vm/index.mjs
@@ -2,10 +2,12 @@ import * as core from "@actions/core"
 import { DefaultAzureCredential } from "@azure/identity"
 import { ComputeManagementClient } from "@azure/arm-compute"
 
-const subId = 'a2b28c85-1948-4263-90ca-bade2bac4df4';
-
 try {
+  const vmName = core.getInput('name')
+  const subId = core.getInput('subscription-id')
   const client = new azCompute.ComputeManagementClient(new DefaultAzureCredential(), subId)
+
+  console.log("Passed.")
 } catch (err) {
   core.setFailed(err.message)
 }

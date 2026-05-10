e7814363d1f7d299448156b439640bd7756c6a3a
alkalax <57093460+alkalax@users.noreply.github.com>
2026-05-10 17:35:37 +0200

    Use start/stop CLI commands

--- golang/vm-booter/cmd/main.go
+++ golang/vm-booter/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 
@@ -8,15 +9,58 @@ import (
 )
 
 func main() {
+	startCmd := flag.NewFlagSet("start", flag.ExitOnError)
+	stopCmd := flag.NewFlagSet("stop", flag.ExitOnError)
+
+	if len(os.Args) < 2 {
+		fmt.Println("error: expected subcommands")
+		os.Exit(1)
+	}
+
 	manager, err := azure.NewVirtualMachineManager("vm_info.json")
 	if err != nil {
 		fmt.Println(err.Error())
 		os.Exit(1)
 	}
 
-	err = manager.StartStopVirtualMachine("ubuntu-0", true)
-	if err != nil {
-		fmt.Println(err.Error())
+	switch os.Args[1] {
+	case "start":
+		startCmd.Parse(os.Args[2:])
+		if startCmd.NArg() < 1 {
+			fmt.Println("error: virtual machine name is required")
+			os.Exit(1)
+		}
+
+		vmName := startCmd.Arg(0)
+		fmt.Printf("Starting VM: %s\n", vmName)
+
+		err = manager.StartStopVirtualMachine(vmName, true)
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Println("Done.")
+	case "stop":
+		stopCmd.Parse(os.Args[2:])
+		if stopCmd.NArg() < 1 {
+			fmt.Println("error: virtual machine name is required")
+			os.Exit(1)
+		}
+
+		vmName := stopCmd.Arg(0)
+		fmt.Printf("Stopping VM: %s\n", vmName)
+
+		err = manager.StartStopVirtualMachine(vmName, false)
+		if err != nil {
+			fmt.Println(err.Error())
+			os.Exit(1)
+		}
+
+		fmt.Println("Done.")
+
+	default:
+		fmt.Println("error: unknown command")
 		os.Exit(1)
 	}
 }

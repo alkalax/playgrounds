211019472a0b488299c1978c7c5c89f161e184e8
alkalax <57093460+alkalax@users.noreply.github.com>
2026-05-20 19:50:58 +0200

    Show shutdown time

--- golang/skip-shutdown-html/main.go
+++ golang/skip-shutdown-html/main.go
@@ -1,10 +1,39 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
 	"os"
+	"time"
 )
+
+type VirtualMachineInfo struct {
+	Name         string
+	ShutdownTime time.Time
+	SkipToday    bool
+}
+
+type PageData struct {
+	VMs          []VirtualMachineInfo
+	SelectedName string
+	Selected     *VirtualMachineInfo
+}
+
+var vms = []VirtualMachineInfo{
+	{Name: "VM1", ShutdownTime: time.Date(2024, 6, 30, 22, 0, 0, 0, time.UTC), SkipToday: false},
+	{Name: "VM2", ShutdownTime: time.Date(2024, 6, 30, 22, 0, 0, 0, time.UTC), SkipToday: true},
+	{Name: "VM3", ShutdownTime: time.Date(2024, 6, 30, 22, 0, 0, 0, time.UTC), SkipToday: false},
+}
+
+func findVM(name string) *VirtualMachineInfo {
+	for i := range vms {
+		if vms[i].Name == name {
+			return &vms[i]
+		}
+	}
+	return nil
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -12,9 +41,21 @@ func main() {
 		port = "8080"
 	}
 
+	tpl := template.Must(template.ParseFiles(
+		"templates/base.html",
+	))
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		log.Printf("Received request: %s %s", r.Method, r.URL.Path)
-		w.Write([]byte("Hello, World!"))
+
+		selectedName := r.URL.Query().Get("vm")
+		pageData := PageData{
+			VMs:          vms,
+			SelectedName: selectedName,
+			Selected:     findVM(selectedName),
+		}
+
+		tpl.ExecuteTemplate(w, "base", pageData)
 	})
 
 	log.Printf("Server is running on port %s", port)

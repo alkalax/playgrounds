4d3e2f904735355d6799c191fa2896e57a7c0084
alkalax <57093460+alkalax@users.noreply.github.com>
2026-05-23 08:55:12 +0200

    Implement repository pattern

--- golang/skip-shutdown-html/internal/storage/mock.go
+++ golang/skip-shutdown-html/internal/storage/mock.go
@@ -1,12 +1,34 @@
-package mock
+package storage
 
 import (
 	"alkalax/skip-shutdown-html/internal/models"
 	"time"
 )
 
-var Vms = []models.VirtualMachineInfo{
-	{Name: "VM1", ShutdownTime: time.Date(2024, 6, 30, 22, 0, 0, 0, time.UTC), SkipToday: false},
-	{Name: "VM2", ShutdownTime: time.Date(2024, 6, 30, 22, 0, 0, 0, time.UTC), SkipToday: true},
-	{Name: "VM3", ShutdownTime: time.Date(2024, 6, 30, 22, 0, 0, 0, time.UTC), SkipToday: false},
+type MockVMRepository struct {
+	vms []models.VirtualMachineInfo
+}
+
+func NewMockVMRepository() *MockVMRepository {
+	return &MockVMRepository{
+		vms: []models.VirtualMachineInfo{
+			{Name: "VM1", ShutdownTime: time.Date(2024, 6, 30, 22, 0, 0, 0, time.UTC), SkipToday: false},
+			{Name: "VM2", ShutdownTime: time.Date(2024, 6, 30, 22, 0, 0, 0, time.UTC), SkipToday: true},
+			{Name: "VM3", ShutdownTime: time.Date(2024, 6, 30, 22, 0, 0, 0, time.UTC), SkipToday: false},
+		},
+	}
+}
+
+func (m *MockVMRepository) GetVMs() []models.VirtualMachineInfo {
+	return m.vms
+}
+
+func (m *MockVMRepository) FindVM(name string) *models.VirtualMachineInfo {
+	for i := range m.vms {
+		if m.vms[i].Name == name {
+			return &m.vms[i]
+		}
+	}
+
+	return nil
 }

ce7f05f9c8cba08b0ad612543796bb7c38a28934
alkalax <57093460+alkalax@users.noreply.github.com>
2026-05-29 12:46:55 +0200

    Add workflow scan action

---
+++ .github/actions/workflow-queue-scan/main.py
@@ -0,0 +1,39 @@
+import os
+import sys
+import github
+
+API_TOKEN = os.getenv("GITHUB_TOKEN")
+if not API_TOKEN:
+    print("Invalid token")
+    sys.exit(1)
+
+auth = github.Auth.Token(API_TOKEN)
+gh = github.Github(auth=auth)
+
+repo = gh.get_repo("alkalax/playgrounds")
+
+print(repo)
+
+runners = repo.get_self_hosted_runners()
+
+matching_runners = []
+for run in repo.get_workflow_runs():
+    if run.status != "completed":
+        print(f"Workflow id: {run.workflow_id}")
+        print(f"Status: {run.status}")
+
+        print("Fetching jobs for the workflow...")
+        for job in run.jobs():
+            if job.labels:
+                print(f"Labels: {job.labels}")
+                for runner in runners:
+                    print(f"Runner: {runner.name}")
+                    print(f"Status: {runner.status}")
+
+                    if set(job.labels).issubset({label['name'] for label in runner.labels}):
+                        print(f"Adding runner {runner.name}")
+                        matching_runners.append(runner)
+            if job.runner_name:
+                print(f"Runner name: {job.runner_name}")
+            if job.runner_group_name:
+                print(f"Runner group name: {job.runner_group_name}")
